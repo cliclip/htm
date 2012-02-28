ee74467dc918023ec67775834185b22b7598daa9
liuxiaodong <liuxiaodong1989@gmail.com>
2012-02-28 10:16:54 +0800

    modify recomment

--- demo/js/clientjs/widget/RecommentWidget.js
+++ demo/js/clientjs/widget/RecommentWidget.js
@@ -2,6 +2,7 @@ RecommentWidget=function(_container,options){
   this.container=_container;
   this.options=options;
   this.widgetType="RecommentWidget";
+  var recommentWidget = this;
   var _view=Backbone.View.extend({
     uid:"",
     el:$(_container),
@@ -13,7 +14,7 @@ RecommentWidget=function(_container,options){
       //var template = _.template($("#recomment_template").html());
       template =  this.template();
       this.el.html(template);
-      this.uid = "";
+      recommentWidget.uid = "";
     },
     events:{
       "click #name_list":"getUserAction",
@@ -25,14 +26,13 @@ RecommentWidget=function(_container,options){
       "focus #text":"clearAction"
     },
     getUserAction:function(evt){
-      uid=evt.target.id;
-      var name=document.getElementById(uid).innerHTML;
-     // var imgurl=document.getElementById(uid).title;
-     // console.log(imgurl);
-     // image.src="../img/search.jpg";
+      recommentWidget.uid=evt.target.id;
+      console.log(recommentWidget.uid);
+      var name=document.getElementById(recommentWidget.uid).innerHTML;
       $("#name").val(name);
       $("#imgId").removeAttr("hidden");
       $("#imgId").attr("src","../img/search.jpg");
+      $("#name_listDiv").empty();
     },
     nameListAction:function(evt){
       var lookup_url=client.URL.HOST_URL + client.SYMBOL.SLASH + client.URL.BASE_URL +"lookup/0..5";
@@ -44,12 +44,17 @@ RecommentWidget=function(_container,options){
 	data:params,
 	successCallBack:function(response){
 	  if(response[0] == 0){
+	    //console.log(response[1]);
+	    if(response[1].length == 1 && response[1][0].name==str){
+	      recommentWidget.uid = response[1][0].uid;
+	    }
 	    $("#name_listDiv").empty();
 	    var name_template = _.template($("#namelist_template").html(),{list:response[1]});
 	    $("#name_listDiv").append(name_template);
 	  }else{
 	    console.log("fail");
-	    //view.el.children().find("span.action-info.name").html(response[1] ? response[1] : "");
+	    var mcode = client.MESSAGES.getErrorMessage(response[1]);
+	    view.el.children().find("span.action-info.user").html(mcode.auth ? mcode.auth : "");
 	  }
 	},
       errorCallBack:function(response){
@@ -67,10 +72,14 @@ RecommentWidget=function(_container,options){
 
     recommentAction:function(evt){
       var clipid="1:1";
-      var recomment_url=client.URL.HOST_URL + client.SYMBOL.SLASH + client.URL.BASE_URL +"user/"+this.uid+"/recomm";
+      var widget=this;
+      console.log(recommentWidget.uid);
+      if(!recommentWidget.uid){
+	widget.el.children().find("span.action-info.user").html("请选择要转发的用户！");
+      }
+      var recomment_url=client.URL.HOST_URL + client.SYMBOL.SLASH + client.URL.BASE_URL +"user/"+recommentWidget.uid+"/recomm";
       var text=$("#text").val();
       var userInfo=new UserInfo(recomment_url);
-      var widget=this;
       userInfo.recommentAction({
 	text:text,
 	clipid:clipid
