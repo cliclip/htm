9b9bab88c6dfec8d831470932eee64523e381015
limjoe <linqiao416@gmail.com>
2012-03-12 14:24:47 +0800

    before userface

--- views/js/clip/app.clipapp.face.js
+++ views/js/clip/app.clipapp.face.js
@@ -1,22 +1,21 @@
 // app.clipapp.face.js
 
 App.ClipApp.Face = (function(App, Backbone, $){
-	var Face = {};
+  var Face = {};
+  Face.showUser = function(uid){
+    if(uid){
+      getUser(uid, function(user){
+	App.vent.trigger("app.clipapp.face:show", user);
+      });
+    }else{
+      App.faceRegion.close();
+    }
+  }
 
-	Face.showUser = function(uid){
-    	if(uid){
-        	getUser(uid, function(user){
-        		App.vent.trigger("app.clipapp.face:show", user);
-        	});
-    	}else{
-    		App.faceRegion.close();
-    	}
-	}
+  App.vent.bind("app.clipapp.face:show", function(user){
+    var faceView = new FaceView({model: user});
+    App.faceRegion.show(faceView);
+  });
 
-	App.vent.bind("app.clipapp.face:show", function(user){
-		var faceView = new FaceView({model: user});
-		App.faceRegion.show(faceView);
-	});
-
-	return Face;
+  return Face;
 })(App, Backbone, jQuery);
