730b374f5b2c6d841e018354b74ba56de2868234
limjoe <linqiao416@gmail.com>
2012-03-13 14:48:40 +0800

    modify reclip recommend commend

--- views/js/clip/app.clipapp.login.js
+++ views/js/clip/app.clipapp.login.js
@@ -89,7 +89,7 @@ App.ClipApp.Login = (function(App, Backbone, $){
 
   // TEST
 
-   App.bind("initialize:after", function(){ Login.open(); });
+  // App.bind("initialize:after", function(){ Login.open(); });
 
   return Login;
 })(App, Backbone, jQuery);
