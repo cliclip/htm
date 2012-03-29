49fd4dcaf1599dae12ba72a1d324ee74f3e4e415
liuxiaodong <liuxiaodong1989@gmail.com>
2012-03-29 10:16:37 +0800

    modify user face

--- views/js/clip/app.clipapp.useredit.js
+++ views/js/clip/app.clipapp.useredit.js
@@ -3,6 +3,7 @@ App.ClipApp.UserEdit = (function(App, Backbone, $){
   var P = App.ClipApp.Url.base;
   var ImgModel = App.Model.extend({});
   var originalFace;
+  var flag = false;
   var LocalImgView = App.ItemView.extend({
     tagName: "form",
     className: "localImg-view",
@@ -36,22 +37,13 @@ App.ClipApp.UserEdit = (function(App, Backbone, $){
   UserEdit.onUploadImgChange = function(sender){
     if( !sender.value.match(/.jpg|.gif|.png|.bmp/i)){
       alert('图片格式无效！');
-      return false;
+      return flag;
     }else{
       var objPreview = document.getElementById('myface-image' );
       if( sender.files &&sender.files[0] ){
 	objPreview.src = window.URL.createObjectURL(sender.files[0]);
+	flag = true;
       }
-    }
-  };
-
-  UserEdit.image_change = function(){
-    var sender = document.getElementById("formUpload");
-    if (!sender.value.match(/.jpg|.gif|.png|.bmp/i)){
-      alert('图片格式无效！');
-      return false;
-    }else{
-      return true;
     }
   };
 
@@ -60,7 +52,7 @@ App.ClipApp.UserEdit = (function(App, Backbone, $){
       url: P+"/user/"+ editModel.id+"/face",
       type: "POST",
       success:function(model,res){
-	App.popRegion.close();
+      UserEdit.close();
 	var uid = editModel.get("id");
 	App.vent.trigger("app.clipapp.face:show",uid);
       },
@@ -95,26 +87,21 @@ App.ClipApp.UserEdit = (function(App, Backbone, $){
 	  var editView = new EditView({model: editModel});
 	  App.popRegion.show(editView);
 	});
-	UserEdit.LocalImgRegion = new App.Region({el:"#faceUploadDiv"});
 	if($("#faceUploadDiv").html() == ""){
 	  $("#post_frame").load(function(){ // 加载图片
 	    var returnVal = this.contentDocument.documentElement.textContent;
 	    if(returnVal != null && returnVal != ""){
 	      var returnObj = eval(returnVal);
 	      if(returnObj[0] == 0){
-		var ids = returnObj[1][0].split(":");
-		var url =  P + "/user/" + ids[0]+ "/image/" + ids[1];
-		var img = $("<img class='face-image' src= "+url+" height='240' width='240'>");
-		$("#userFace").empty();
-		$("#userFace").append(img);
 		var currentFace = returnObj[1][0];
-		if(currentFace){
-		  var editmodel = new EditModel({id:user});
-		  if(originalFace){
-		    UserEdit.removeFace(editmodel,originalFace);
-		  }
-		  UserEdit.saveFace(editmodel,{face:currentFace});
+		if(!flag){ //flag为true时图片改变并有效，为false时图片没改变或者无效
+		  currentFace = originalFace;
 		}
+		var editmodel = new EditModel({id:user});
+		if(originalFace){
+		  UserEdit.removeFace(editmodel,originalFace);
+		}
+		UserEdit.saveFace(editmodel,{face:currentFace});
 	      }
 	    }
 	  });
@@ -126,6 +113,7 @@ App.ClipApp.UserEdit = (function(App, Backbone, $){
 
   UserEdit.close = function(){
     App.popRegion.close();
+    flag = false;//关闭头像设置时把flag赋为初始化的值
   };
 
   App.vent.bind("app.clipapp.useredit:show",function(){
