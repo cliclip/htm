a79300f00b68df2f7e60c88f4d0c6fae37b8a8ac
wushijing <wushijing2011@gmail.com>
2012-04-13 11:35:15 +0800

    保持气泡和用户名部分的锁定

--- views/js/clip/app.clipapp.util.js
+++ views/js/clip/app.clipapp.util.js
@@ -153,13 +153,15 @@ App.util = (function(){
       var st = $(window).scrollTop();
       var wh = window.innerHeight;
       // fix left while scroll
-      var mt = $(".clearfix").offset().top + $("#face").height();
-      if($("#list").height()<=175)return;
+      var mt = $(".clearfix").offset().top + 240;//$("#face").height();
+      if($("#list").height()<=500)return;
       if(st > mt ){
-	$("#bubb").addClass("fixed").css({"margin-top": "0px", "top": paddingTop+"px"});
+	$(".user_detail").addClass("fixed").css({"margin-top": "0px", "top": paddingTop+"px"});
+	$("#bubb").addClass("fixed").css({"margin-top": $(".user_detail").height()+"px", "top": paddingTop+"px"});
 	$(".return_top").show();
 	// show go-top while scroll
       } else {
+	$(".user_detail").removeClass("fixed").css("margin-top", paddingTop+"px");
 	$("#bubb").removeClass("fixed").css("margin-top", paddingTop+"px");
 	$(".return_top").hide();
       }
