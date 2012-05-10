26e7f1c804f29302d76f3d471cc387400c5ba0c5
limjoe <linqiao416@gmail.com>
2012-05-10 10:43:20 +0800

    modify namelist

--- views/js/clip/app.clipapp.recommend.js
+++ views/js/clip/app.clipapp.recommend.js
@@ -30,12 +30,13 @@ App.ClipApp.Recommend = (function(App,Backbone,$){
     template:"#recommend-view-template",
     events:{
       "click .user" : "getUserAction",
-      "input #recomm_name": "nameListAction",
-      "click #recomm_name": "nameListAction",
-      "focus #recomm_name": "nameListShow",
-      "blur #recomm_name" : "nameBlur",
-      "mouseover .user"   :  "MouseOver",
-      "mouseout .user"    :  "MouseOut",
+      "input #recomm_name"   : "nameListAction",
+      "click #recomm_name"   : "nameListAction",
+      "focus #recomm_name"   : "nameListShow",
+      "blur #recomm_name"    : "nameBlur",
+      "keypress #recomm_name": "selectAction",
+      "mouseover .user"      :  "MouseOver",
+      "mouseout .user"       :  "MouseOut",
       "focus #recommend_text":  "clearAction",
       "blur  #recommend_text":  "textBlur",
       "click #submit"        :  "recommendAction",
@@ -85,7 +86,7 @@ App.ClipApp.Recommend = (function(App,Backbone,$){
 	      data.id=li[1].id.split("_")[1];
 	    }
 	  });
-	  $(".name_list").hide();
+	  $(".list").remove();
 	}
 	// 先根据 data[name]找到uid，在进行model的新建和name的set
 	view.tmpmodel.set(data, {
@@ -95,6 +96,49 @@ App.ClipApp.Recommend = (function(App,Backbone,$){
 	});
 	$(".name_list").hide();
       },200);
+    },
+    selectAction:function(event){
+      if(event.keyCode == 40){
+	var flag = true;
+	var div = $("#name_listDiv").children().children().children();
+	for(var i=0;i<div.length/2;i++){
+	  if(flag && $(div[2*i+1]).css("background-color") == "rgb(136, 136, 136)"){
+	    $(div[2*i+1]).css("background-color","");
+	    $(div[2*(i+1)+1]).css("background-color","#888");
+	    $("#recomm_name").val($(div[2*(i+1)+1]).text());
+	    flag = false;
+	  }
+	}
+	if(flag){
+	  $(div[1]).css("background-color","#888");
+	  $("#recomm_name").val($(div[1]).text());
+	}
+      }else if(event.keyCode == 38){
+	var flag = true;
+	var div = $("#name_listDiv").children().children().children();
+	for(var i=0;i<div.length/2;i++){
+	  if(flag && $(div[2*i+1]).css("background-color") == "rgb(136, 136, 136)"){
+	    $(div[2*i+1]).css("background-color","");
+	    $(div[2*i-1]).css("background-color","#888");
+	    $("#recomm_name").val($(div[2*i-1]).text());
+	    flag = false;
+	  }
+	}
+	if(flag){
+	  $(div[div.length-1]).css("background-color","#888");
+	  $("#recomm_name").val($(div[length-1]).text());
+	}
+      }else if(event.keyCode == 13){
+	var div = $("#name_listDiv").children().children().children();
+	for(var i=0;i<div.length/2;i++){
+	  if($(div[2*i+1]).css("background-color") == "rgb(136, 136, 136)"){
+	    $("#recomm_name").val($(div[2*i+1]).text());
+	    $("#imgId").attr("src",div[2*i].children[0].src);
+	    $("#imgId").css("display","block");
+	    $("#recommend_text").focus();
+	  }
+	}
+	}
     },
     nameListShow:function(e){
       this.cleanError(e);
