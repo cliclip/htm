8afbb0f386d0fae6208f8df00fa2a7ac3212e936
heting <heting0825@126.com>
2012-08-28 16:13:01 +0800

    weibo绑定事件触发以及提示信息更正

--- views/js/clip/app.clipapp.oauth.js
+++ views/js/clip/app.clipapp.oauth.js
@@ -1,33 +1,26 @@
 // app.clipapp.oauth.js
-
 App.ClipApp.Oauth = (function(App, Backbone, $){
   var Oauth = {};
   Oauth.process=function(){
     checkUser(function(err,res){
-      if(err&&err.hasbind_err){
-	App.ClipApp.showConfirm("account_hasbind");
-	Backbone.history.navigate("my",true);
-      }else if(res&&res.oauth){
-	App.ClipApp.showUserBind(res.oauth);
-      }else if(res&&res.token){
-	/*if(res.provider == "weibo"){ // 已经绑定成功直接登录就好
-	  App.ClipApp.showConfirm("weibo_sucmsg",res.name);
-	}else if(res.provider == "twitter"){
-	  App.ClipApp.showConfirm("twitter_sucmsg",res.name);
-	}*/
-	delete res.provider;
-	delete res.name;
-	App.vent.trigger("app.clipapp.login:getedToken",res);
-	setTimeout(function(){
+      if(err && err.hasbind_err){
+	var sure = function(){
+	  Backbone.history.navigate("my", true);
 	  App.ClipApp.showUserEdit();
-	},0);
+	};
+	var cancel = function(){ Backbone.history.navigate("my", true); };
+	App.ClipApp.showAlert("account_hasbind", null, sure, cancel);
+      }else if(res && res.oauth){
+	App.ClipApp.showUserBind(res.oauth);
+      }else if(res && res.token){
+	App.vent.trigger("app.clipapp.login:getedToken",res);
       }else{
-	App.ClipApp.showAlert("oauth_fail",null,function(){
-	  if(App.ClipApp.isLoggedIn())  Backbone.history.navigate("my",true);
-	  else    Backbone.history.navigate("register",true);
-	},function(){
-	  Backbone.history.navigate("",true);
-	});
+	var sure = function(){
+	  App.ClipApp.showUserEdit();
+	  Backbone.history.navigate("my", true);
+	};
+	var cancel = function(){ Backbone.history.navigate("my", true); };
+	App.ClipApp.showAlert("oauth_fail",null, sure, cancel);
       }
     });
   };
