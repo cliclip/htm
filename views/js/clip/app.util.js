c6d8e6682630a38313015fe14ed8c2b8193ab659
liuxiaodong <liuxiaodong1989@gmail.com>
2012-11-08 17:43:59 +0800

    fetch team

--- views/js/clip/app.util.js
+++ views/js/clip/app.util.js
@@ -194,7 +194,7 @@ App.util = (function(){
    */
   util.cleanConImgUrl = function(content){
     var str1 = "src=\\'",str2 = 'src=\\"';
-    var str3 = "http://(192\\.168\\.1\\.3|cliclip\\.com)(:(\\d{4}))?";
+    var str3 = "http://(192\\.168\\.1\\.3|cliclip\\.com)(:(\\d{1,5}))?";
     var reg0 = /\/_3_\//g,reg = /(\d+)\/clip_(\d+)_/g;
     // 匹配图片src为http:192.168.1.3:....以及cliclip.com(:....)
     var reg1 = new RegExp(str1 + str3,"g");
