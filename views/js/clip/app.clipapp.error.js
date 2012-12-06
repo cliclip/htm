fdb5ae6fd2c1b781185724541dd3fafd59cd60bf
limjoe <linqiao416@gmail.com>
2012-12-06 11:41:13 +0800

    error process

--- views/js/clip/app.clipapp.error.js
+++ views/js/clip/app.clipapp.error.js
@@ -1,14 +1,19 @@
 App.ClipApp.Error=(function(App,Backbone,$){
  var Error = {};
  Error.process=function(message){
-   // console.log(typeof(message));
    if(message == "InternalOAuthError"){
-     App.ClipApp.showConfirm(message);
+     var sure = function(){
+       if(App.ClipApp.isLoggedIn()){
+	 App.ClipApp.showUserEdit();
+	 Backbone.history.navigate("my", true);
+       }else Backbone.history.navigate("register",true);
+     };
+     App.ClipApp.showConfirm(message,null, sure);
    }else{
      App.ClipApp.showConfirm("error_message");
+     if(App.ClipApp.isLoggedIn())  Backbone.history.navigate("my",true);
+     else Backbone.history.navigate("register",true);
    }
-  if(App.ClipApp.isLoggedIn())  Backbone.history.navigate("my",true);
-  else Backbone.history.navigate("register",true);
   };
   return Error;
 })(App,Backbone,jQuery);
